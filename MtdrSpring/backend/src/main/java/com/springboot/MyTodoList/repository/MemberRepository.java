3172119d51bf4f9da2afb63a633c887898e0eaae
Alan Ruelas <A01641426@tec.mx>
2024-05-26 23:43:18 -0600

    Configurations changed to local development

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/MemberRepository.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/repository/MemberRepository.java
@@ -1,7 +1,7 @@
 package com.springboot.MyTodoList.repository;
 
 import com.springboot.MyTodoList.model.Member;
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
