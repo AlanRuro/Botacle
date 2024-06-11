d01cebf4eb879d3c2a27ab0bf83f0cd3d02c0ecb
Alan Ruelas <A01641426@tec.mx>
2024-06-10 19:14:50 -0600

    Delete task functions that were commented are ereased

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemBotController.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/controller/ToDoItemBotController.java
@@ -428,18 +428,6 @@ public class ToDoItemBotController extends TelegramLongPollingBot {
         send(chatId, welcomeMessage);
     }
 
-    private void sendWelcomManagerMessage(long chatId) {
-        String welcomeMessage = "🤖 ¡Hola! Soy Botacle, tu bot de lista de tareas. Aquí están los comandos que puedes usar:\n\n" +
-                "📝 /start - Iniciar y obtener un resumen\n" +
-                "📋 /todolist - Ver tu lista de tareas\n" +
-                "➕ /additem - Añadir una nueva tarea\n" +
-                "👥 /employeeslist - Ver las tareas de tus empleados\n" +
-                "❌ /cancel - Cancelar la acción actual\n\n" +
-                "¡Espero ayudarte a mantenerte organizado!";
-
-        send(chatId, welcomeMessage);
-    }
-
     private void sendWelcomeMessage(long chatId) {
         String welcomeMessage = "🤖 ¡Hola! Soy Botacle, tu bot de lista de tareas. Aquí están los comandos que puedes usar:\n\n" +
                 "📝 /start - Iniciar y obtener un resumen\n" +
