df8f709af56ee1a82a2cf27131b1c5b7d39fa9b3
Alan Ruelas <A01641426@tec.mx>
2024-06-11 22:29:23 -0600

    Commands message moved to BotMessages

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotMessages.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/util/BotMessages.java
@@ -2,7 +2,19 @@ package com.springboot.MyTodoList.util;
 
 public enum BotMessages {
 	
-	HELLO_MYTODO_BOT("Hola! Soy un bot para que los empleados de Oracle puedan organizarse mejor!\nPara usarme por favor usa el comando /login."),
+	HELLO_MANAGER("🤖 ¡Hola! Soy Botacle, tu bot de lista de tareas. Aquí están los comandos que puedes usar:\n\n"
+                + "📝 /start - Iniciar y obtener un resumen\n"
+                + "📋 /todolist - Ver tu lista de tareas\n"
+                + "➕ /additem - Añadir una nueva tarea\n"
+                + "👥 /employeeslist - Ver las tareas de tus empleados\n"
+                + "❌ /cancel - Cancelar la acción actual\n\n"
+                + "¡Espero ayudarte a mantenerte organizado!"),
+        HELLO_MEMBER("🤖 ¡Hola! Soy Botacle, tu bot de lista de tareas. Aquí están los comandos que puedes usar:\n\n"
+                + "📝 /start - Iniciar y obtener un resumen\n"
+                + "📋 /todolist - Ver tu lista de tareas\n"
+                + "➕ /additem - Añadir una nueva tarea\n"
+                + "❌ /cancel - Cancelar la acción actual\n\n"
+                + "¡Espero ayudarte a mantenerte organizado!"),
         SUMMARY("Puedes usar estos comandos:\n/todolist para ver tus tareas\n/additem para agregar una tarea\n/logout para cerrar sesion"),
 	BOT_REGISTERED_STARTED("Bot registered and started succesfully!"),
         LOGIN("Por favor, usa /login para iniciar sesión."),
@@ -11,7 +23,7 @@ public enum BotMessages {
 	ITEM_DELETED("Item deleted! Select /todolist to return to the list of todo items, or /start to go to the main screen."),
 	TYPE_NEW_TODO_ITEM("Ingresa el nombre de la tarea"),
 	NEW_ITEM_ADDED("New item added! Select /todolist to return to the list of todo items, or /start to go to the main screen."),
-        UNKNOWN_TEXT("Para usar este bot debe iniciar sesion con /login."),
+        UNKNOWN_TEXT("Ingrese un comando valido. Para ver la lista de comando escriba /start"),
         NOT_MEMBER("No eres usuario"),
 	BYE("Bye! Select /start to resume!");
 
