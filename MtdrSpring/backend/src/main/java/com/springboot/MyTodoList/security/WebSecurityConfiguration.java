535e3faa4fe1470385aab28495a23ca0cc44507d
Alan Ruelas <A01641426@tec.mx>
2024-06-13 01:18:26 -0600

    Web Configuration file package line uncommented

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/security/WebSecurityConfiguration.java
@@ -1,5 +1,5 @@
-//package com.springboot.MyTodoList.security;
-//
+package com.springboot.MyTodoList.security;
+
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
